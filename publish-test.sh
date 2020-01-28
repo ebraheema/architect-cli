088c1aa9cbd456c12dcfd91ce672fcf7a664d060
Ryan Cahill <ryan.cahill444@gmail.com>
2020-01-28 11:54:05 -0500

    updating

--- publish-test.sh
+++ publish-test.sh
@@ -2,6 +2,6 @@ git add .
 git commit -m "updating"
 git push origin tag-testing
 git push --delete origin v1.0.0
-git tag -d v1-test
-git tag -a v1-test -m "testing binary uploads"
-git push origin v1-test
+git tag -d v1.0.0
+git tag -a v1.0.0 -m "testing binary uploads"
+git push origin v1.0.0
